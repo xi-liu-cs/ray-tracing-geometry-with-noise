b69ba21274a83d8cf11da3b134f04e11f017e789
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-09-22 11:18:55 -0400

    Add files via upload

--- js/lib3D.js
+++ js/lib3D.js
@@ -5,17 +5,29 @@ const VERTEX_SIZE = 3;
 
 let shaderHeader = `
 precision highp float;
-float noise(vec3 v) {
+float noise(vec3 v)
+{
    vec4 r[2];
-   const mat4 E = mat4(0.,0.,0.,0., 0.,.5,.5,0., .5,0.,.5,0., .5,.5,0.,0.);
-   for (int j = 0 ; j < 2 ; j++)
-   for (int i = 0 ; i < 4 ; i++) {
-      vec3 p = .60*v + E[i].xyz, C = floor(p), P = p - C-.5, A = abs(P), D;
-      C += mod(C.x+C.y+C.z+float(j),2.) * step(max(A.yzx,A.zxy),A)*sign(P);
-      D  = 314.1*sin(59.2*float(i+4*j) + 65.3*C + 58.9*C.yzx + 79.3*C.zxy);
-      r[j][i] = dot(P=p-C-.5,fract(D)-.5) * pow(max(0.,1.-2.*dot(P,P)),4.);
-   }
-   return 6.50 * (r[0].x+r[0].y+r[0].z+r[0].w+r[1].x+r[1].y+r[1].z+r[1].w);
+   const mat4 E = mat4
+   (
+      0., 0., 0., 0.,
+      0., .5, .5, 0.,
+      .5, 0., .5, 0.,
+      .5, .5, 0., 0.
+   );
+   for(int j = 0 ; j < 2 ; j++)
+      for(int i = 0 ; i < 4 ; i++)
+      {
+         vec3 p0 = .60 * v + E[i].xyz,
+         c = floor(p0),
+         p = p0 - c -.5,
+         a = abs(p), d;
+         c += mod(c.x + c.y + c.z + float(j), 2.) * step(max(a.yzx, a.zxy), a) * sign(p);
+         d  = 314.1 * sin(59.2 * float(i + 4 * j) + 65.3 * c + 58.9 * c.yzx + 79.3 * c.zxy);
+         p = p0 - c - .5;
+         r[j][i] = dot(p, fract(d) - .5) * pow(max(0., 1. - 2. * dot(p, p)), 4.);
+      }
+   return 6.50 * (r[0].x + r[0].y + r[0].z + r[0].w + r[1].x + r[1].y + r[1].z + r[1].w);
 }`;
 
 addEventListenersToCanvas = function(canvas) {
